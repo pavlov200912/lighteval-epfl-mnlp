26540e3be3f8bce94b1cc9a67db0c3c5b03a45fc
Zeming Chen <zeming.chen@epfl.ch>
2025-04-15 03:34:27 +0200

    setup mnlp custom task examples under community

--- community_tasks/mnlp_mcqa_evals.py
+++ community_tasks/mnlp_mcqa_evals.py
@@ -1,42 +1,15 @@
-# MIT License
-
-# Copyright (c) 2024 The HuggingFace Team
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# ruff: noqa: F405, F403, F401
-
 from lighteval.tasks.requests import Doc
 from lighteval.metrics.metrics import Metrics
 from lighteval.tasks.default_prompts import LETTER_INDICES
 from lighteval.tasks.lighteval_task import LightevalTaskConfig
 
 def mmlu_harness(line, task_name: str = None):
-    # topic = line["subject"]
-    topic = "advanced master-level STEM courses"
+    topic = "knowledge and kills in advanced master-level STEM courses"
     prompt = f"The following are multiple choice questions (with answers) about {topic.replace('_', ' ')}.\n\n"
     prompt += line["question"] + "\n"
     prompt += "".join([f"{key}. {choice}\n" for key, choice in zip(LETTER_INDICES, line["choices"])])
     prompt += "Answer:"
-
-    gold_ix = LETTER_INDICES.index(line["answer"]) if isinstance(line["answer"], str) else line["answer"]
-    "__few_shots" in line and line["__few_shots"] is True  # We are adding few shots
+    gold_ix = LETTER_INDICES.index(line["answer"])
 
     return Doc(
         task_name=task_name,
@@ -51,11 +24,9 @@ task = LightevalTaskConfig(
     prompt_function=mmlu_harness,
     suite=["community"],
     hf_subset="",
-    hf_repo="zechen-nlp/MNLP_mcqa_test",
+    hf_repo="zechen-nlp/MNLP_STEM_mcqa_evals",
     hf_avail_splits=["test"],
     evaluation_splits=["test"],
-    # few_shots_split="test",
-    # few_shots_select="sequential",
     metric=[Metrics.loglikelihood_acc, Metrics.loglikelihood_acc_norm_nospace],
     generation_size=-1,
     stop_sequence=None,
