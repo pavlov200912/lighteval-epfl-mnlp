95cc54427b676d51f2bcaa9e7cad3e7141a7091e
Zeming Chen <zeming.chen@epfl.ch>
2025-05-08 00:06:19 +0200

    configure ref_free_norm argument

--- src/lighteval/main_accelerate.py
+++ src/lighteval/main_accelerate.py
@@ -186,10 +186,8 @@ def accelerate(  # noqa C901
         args_dict["accelerator"] = accelerator
         args_dict["quantization_config"] = quantization_config
         args_dict["batch_size"] = override_batch_size
-        # args_dict["multichoice_continuations_start_space"] = config["base_params"][
-        #     "multichoice_continuations_start_space"
-        # ]
-        args_dict["multichoice_continuations_start_space"] = None
+        args_dict["ref_free_norm"] = config["base_params"]["ref_free_norm"]
+        args_dict["multichoice_continuations_start_space"] = True
         args_dict["use_chat_template"] = use_chat_template
 
         # Keeping only non null params
