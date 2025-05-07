479c8e2e689be629da6eb83d8d3bc9adc1785e76
Zeming (Eric) Chen <zeming.chen@epfl.ch>
2025-05-07 21:26:27 +0200

    Update mnlp_dpo_evals.py

--- community_tasks/mnlp_dpo_evals.py
+++ community_tasks/mnlp_dpo_evals.py
@@ -18,15 +18,14 @@ task = LightevalTaskConfig(
     prompt_function=preference_pair,
     suite=["community"],
     hf_subset="",
-    hf_repo="zechen-nlp/MNLP_dpo_demo",
+    hf_repo="zechen-nlp/MNLP_dpo_demo",   # Change the repo name to the evaluation dataset that you compiled
     hf_avail_splits=["test"],
     evaluation_splits=["test"],
     metric=[Metrics.reward_model_acc],
     generation_size=-1,
     stop_sequence=None,
     trust_dataset=True,
-    limited_num_samples=10
-    # Set to 0 to use all samples, specify a number to limit the number of samples for debugging purpose
+    limited_num_samples=10 # Set to 0 to use all samples, specify a number to limit the number of samples for debugging purpose
 )
 
 # STORE YOUR EVALS
